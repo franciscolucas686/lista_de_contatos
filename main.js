ec73b114c559ff50ab2771b2f4ec290da1adff7b
Francisco Lucas <franciscolucas686@gmail.com>
2024-02-28 23:03:36 -0300

    Lógica criada com JS

--- main.js
+++ main.js
@@ -1,17 +1,52 @@
+const form = document.getElementById('form-contato');
+const inputNomeContato = document.getElementById('nome');
 const inputNumeroTelefone = document.getElementById('telefone');
+const contatos = [];
+let linhas = '';
 
-inputNumeroTelefone.addEventListener('keyup', function(){
+inputNumeroTelefone.addEventListener('input', limitarDigitos);
+
+function limitarDigitos(){
     const limiteCaracteres = 11;
     const numeroTelefone = inputNumeroTelefone.value;
-
-    if(numeroTelefone.length > limiteCaracteres){
-
-        numeroTelefone.slice(0, limiteCaracteres)
-
+    
+    if(numeroTelefone.length >= limiteCaracteres) {
+        inputNumeroTelefone.value = numeroTelefone.slice(0, limiteCaracteres)
     }
+};
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    adicionarLinha();
+    atualizaTabela();
+    totalContatos();
+    atualizaTabelaTotalContatos();
+    limpaInput();
 })
 
 
+function adicionarLinha(){
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumeroTelefone.value}</td>`;
+    linha += '</tr>';
+    linhas += linha;
+}
+
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function totalContatos(){
+    contatos.push(inputNomeContato.value);
+}
+
+function atualizaTabelaTotalContatos(){    
+    document.getElementById('numero-total-contatos').innerHTML = contatos.length;
+}
 
 
 
@@ -29,14 +64,7 @@ inputNumeroTelefone.addEventListener('keyup', function(){
 
 
 
-
-
-
-
-// const form =document.getElementById('form-contato');
-// form.addEventListener('submit', function(e){
-//     e.preventDefault();
-
-//     const inputNomeContato = document.getElementById('nome')
-    
-// })
+function limpaInput(){
+    inputNomeContato.value = '';
+    inputNumeroTelefone.value = '';
+}
